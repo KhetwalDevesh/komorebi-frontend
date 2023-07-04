e69f22a692b3988666ea9372d1a6040d750a757c
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-07-04 11:52:00 +0530

    added payment success link in prod

--- src/components/checkout-form.tsx
+++ src/components/checkout-form.tsx
@@ -59,7 +59,7 @@ export default function CheckoutForm() {
 			elements,
 			confirmParams: {
 				// Make sure to change this to your payment completion page
-				return_url: "http://127.0.0.1:5173/success",
+				return_url: "https://komorebi-frontend.vercel.app/success",
 			},
 		});
 
